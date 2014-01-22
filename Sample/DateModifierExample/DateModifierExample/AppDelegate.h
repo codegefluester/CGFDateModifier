376c601feedf98fb6793c2add1853985f2faf5f1
codegefluester <bjoern@codegefluester.de>
2014-01-22 17:06:33 +0100

    Sample project initial

---
+++ Sample/DateModifierExample/DateModifierExample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DateModifierExample
+//
+//  Created by Björn Kaiser on 22.01.14.
+//  Copyright (c) 2014 Björn Kaiser. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Sample/DateModifierExample/DateModifierExample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DateModifierExample
+//
+//  Created by Björn Kaiser on 22.01.14.
+//  Copyright (c) 2014 Björn Kaiser. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
